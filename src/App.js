a18032bbeb902ce2e014b3a7cfb464899127ed0c
jcompanion <joshua.companion@g.austincc.edu>
2021-06-04 21:45:06 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+import React from "react";
+import HomePage from "./HomePage"
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+      
+    </div>
+  );
+}
+
+export default App;
